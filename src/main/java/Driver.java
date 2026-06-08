d828d3db6d6f6e20ac2d5580e32dc13a85842599
Nadya Aulia Salma <nadyaauliasalma622@gmail.com>
2026-06-08 08:05:06 +0700

    Add second Counter instance and increment logic
    Added a second Counter instance and incremented it multiple times. Included a conditional block that will never execute.

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -5,16 +5,26 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Counter counter = new Counter();
+		Counter counter2 = new Counter(); 
+		int x = 0; 
 		
 		System.out.println("Current count:" + counter.getCount());
 		
 		counter.increment();
+		counter.increment();
+		counter.increment();
+		counter.increment();
+		counter.increment(); 
 		
 		System.out.println("Current count:" + counter.getCount());
 		
 		counter.decrement();
 		
 		System.out.println("Current count:" + counter.getCount());
+		
+		if (false) {
+			System.out.println("ini tidak akan pernah dieksekusi");
+		}
 		
 	}
 
